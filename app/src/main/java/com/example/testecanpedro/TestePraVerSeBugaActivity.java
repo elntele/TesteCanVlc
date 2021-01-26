8140ea14194bdab6c53a0e66dff0b941e8d1c216
elntele <elntele@gmail.com>
2021-01-26 01:36:45 -0300

    commit incial

---
+++ app/src/main/java/com/example/testecanpedro/TestePraVerSeBugaActivity.java
@@ -0,0 +1,14 @@
+package com.example.testecanpedro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestePraVerSeBugaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste_pra_ver_se_buga);
+    }
+}
